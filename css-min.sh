e598be340487e6b113a8e1a31675786c65853596
Lauri Rooden <lauri@rooden.ee>
2012-02-08 14:54:05 +0200

    Performance improvements
    Squashed commit of the following:
    
    commit ec4903e37ce70ce811ffc50135b20c57ec35d10b
    Merge: 97590fb 6400304
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Wed Feb 8 14:53:12 2012 +0200
    
        Merge branch 'maint' into devel
    
    commit 6400304ddfb0d8d59bb4d36bb1da2b20038e7e8b
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Tue Feb 7 17:04:35 2012 +0200
    
        Remove some expr call
    
    commit 97590fb71478cfc80a47d4c73c1e18c3b25242ff
    Merge: 237c5ba caef526
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Feb 6 11:33:25 2012 +0200
    
        Merge branch 'maint' into devel
    
    commit caef526b7e3a54149acadbc53be77e556ce5fc73
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Feb 6 11:33:15 2012 +0200
    
        Add comments
    
    commit 7ddc20afe298a4f49da19a1412ab33158ae34a7a
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Feb 6 11:20:47 2012 +0200
    
        data-uri without sed
    
    commit c50e8b9b83d401b2acae5a30e491c8bcbdcab5c0
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Feb 6 10:46:46 2012 +0200
    
        Remove get_pos
    
    commit 237c5ba83f7e18aaa89d7c934d60ea5d9353f485
    Merge: 0fbbe23 7f748dd
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Feb 6 10:18:04 2012 +0200
    
        Merge branch 'maint' into devel
    
    commit 7f748ddf9a527bb7c7f987ced17a627a884f5be6
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Feb 6 00:09:45 2012 +0200
    
        Remove NAMES array
    
    commit ed38e5e5ef0e170ecc703909cfb0691b7fcb3974
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Sun Feb 5 23:39:51 2012 +0200
    
        Read sprites.txt once
    
    commit dca9130d0d82bcbc612cd1103b0f6fb81d5fa76e
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Jan 30 17:49:31 2012 +0200
    
        new 'usage' prefix
    
    commit 976cc7e6132d704c0308dc57bdf3e25ff8fb4efb
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Mon Jan 30 11:23:44 2012 +0200
    
        Fix css sprite positive position
    
    commit 0fbbe239cd1d9a8cf661db7fb0bd6c083bab83c7
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Thu Jan 26 12:32:45 2012 +0200
    
        Remove CSS comments after import
    
    commit bd9b287a0eefb4b4e103f265195c6eb68f3801d4
    Author: Lauri Rooden <lauri@rooden.ee>
    Date:   Thu Jan 26 12:21:16 2012 +0200
    
        Fix css_import scope issues

--- css-min.sh
+++ css-min.sh
@@ -29,25 +29,18 @@
 
 export LC_ALL=C
 
-NAMES=()
+unquote(){
+	local a="$1"
+	a="${a#\"}";a="${a%\"}";a="${a#\'}"
+	echo ${a%\'}
+}
 
-while getopts ':l:s' OPT; do
-	case $OPT in
-		# insert license file
-		l)  sed -e 's/^/ * /' -e '1i/**' -e '$a\ *\/' $OPTARG;;
-
-		# reset sprite images
-		s)  rm -f sprite.txt;;
-
-		:)  echo "Option -$OPTARG requires an argument." >&2; exit 1;;
-		\?) echo "Invalid option: -$OPTARG" >&2; exit 1;;
-	esac
-done
-
-shift $((OPTIND-1))
 
 file_in_url() {
-	expr -- "$1" : ".*url(['\"]*\([^'\")]*\)"
+	local a="$1"
+	a="${a#*url(}"
+	unquote "${a%)*}"
+	#expr -- "$1" : ".*url(['\"]*\([^'\")]*\)"
 }
 
 clean_dirname() {
@@ -56,67 +49,112 @@ clean_dirname() {
 
 css_import() {
 	while read s; do
-		case "$s" in
-			"@import "*)
+		[[ "$s" = "@import "* ]] && {
 				file=$(file_in_url "$s")
 				sed -E -e "s,url\(['\"]*,&$(clean_dirname "$file"),g" "$file" | css_import
-				;;
+		} || echo "$s"
+	done
+}
+
+
+
+# A license may be specified with the `-l` option.
+test "$1" = '-l' && {
+	sed -e 's/^/ * /' -e '1i/**' -e '$a\ *\/' "$2"
+	shift;shift
+}
+
+
+
+# Import CSS files specified in arguments
+for a in "$@"; do echo "@import url('$a');"; done | css_import |
+
+# remove non-important comments
+sed -E \
+    -e '/\/\*([^@!]|$)/ba' -e b  -e :a \
+    -e 's,/\*[^@!]([^*]|\*[^/])*\*/,,g;t' \
+    -e 'N;ba' |
+
+# replace data-uri's and sprites
+{
+	POS=($(cat sprite.txt 2>/dev/null))
+	UPDATED=()
+
+	while read s; do
+		case "$s" in
 			*"/*! data-uri */")
+				# Remove comment
+				s="${s%%/\**}"
+
 				file=$(file_in_url "$s")
+				echo "${s%%$file*}data:image/${file##*.};base64,$(base64 -w0 $file)${s##*$file}"
 				#data=$(openssl enc -a -in $a | tr -d "\n")
-				s=$(echo "$s" | sed "s:$file:%s:;s:/\*.*$::")
-				printf "$s" "data:image/${file##*.};base64,$(base64 -w0 $file)"
 				;;
 			*"/*! sprite "*)
 				file=$(file_in_url "$s")
-				name="$(expr -- "$s" : ".*sprite \([[:alpha:]]*\)").png"
-				pos=$(sed -n "/ ${file//\//\\/}/s/ .*//p" sprite.txt 2>/dev/null)
+
+				# Extract sprite name from comment
+				name="${s##*sprite }";name="${name%% *}.png"
+
+				# Remove comment
+				s="${s%%/\**}"
+
+				pos=""
+				for item in "${POS[@]}"; do
+					[[ "$item" = *":$file:"* ]] && pos=${item%%:*} && break
+				done
 				if [ -z "$pos" ]; then
-					if [[ "${NAMES[@]}" = *"$name"* ]]; then
+					if [[ " ${POS[@]} " = *":$name "* ]]; then
 						pos=$(identify -format "%h" "$name")
+
+						# TODO:2012-01-30:lauriro: 1px gap between sprite parts is needed to work around zoom errors (at least in IE).
 						convert "$name" "$file" -append PNG8:"$name"
 					else
 						pos=0
 						convert "$file" PNG8:"$name"
-						NAMES=("${NAMES[@]}" "$name")
 					fi
-					echo "$pos $file" >> sprite.txt
+
+					POS=("${POS[@]}" "$pos:$file:$name")
+					[[ " ${UPDATED[@]} " = *" $name "* ]] || UPDATED=("${UPDATED[@]}" "$name")
 				fi
-				echo "$s" | sed "s:/\*.*$::;T;s:url([^)]*:url($name:;T;s:px 0px:px ${pos}px:;t;s:top:${pos}px:;t;s:):) 0px ${pos}px:"
+				echo "$s" | sed "s:url([^)]*:url($name:;T;s:px 0px:px -${pos}px:;t;s:top:-${pos}px:;t;s:):) 0px -${pos}px:"
 				;;
 			*)
 				echo "$s"
 				;;
 		esac
 	done
-}
 
+	echo "${POS[@]}" > sprite.txt
 
-for a in "$@"; do
-	echo "@import url('$a');" | css_import
-done |
+	for name in "${UPDATED[@]}"; do
+		echo "pngcrush: $name" >&2
+		pngcrush -rem allb -brute -reduce "$name" _.png >/dev/null && mv -f _.png "$name"
+	done
 
-# remove comments BSD safe
-sed -E \
-    -e '/\/\*([^@!]|$)/ba' -e b  -e :a \
-    -e 's,/\*[^@!]([^*]|\*[^/])*\*/,,g;t' \
-    -e 'N;ba' |
+	#pngcrush -rem allb -brute -reduce original.png optimized.png
+	#optipng -o7 original.png
 
-tr -s "\t\n " " " | tr "'" '"' |
+} |
 
+tr "'\t\n" '"  ' |
+
+# Remove spaces and put each rule to separated line
 sed -E \
     -e 's/ *([,;{}]) */\1/g' \
     -e 's/^ *//' \
     -e 's/;*}/}\
 /g' |
 
+
+# Use CSS shorthands
 sed -E \
     -e '/(^|\{\})$/d' \
     -e 's/ and\(/ and (/g;t' \
     -e 's/: */:/g' \
-    -e 's/([^0-9])0(px|em|%|in|cm|mm|pc|pt|ex)/\10/g' \
+    -e 's/([^0-9])-?0(px|em|%|in|cm|mm|pc|pt|ex)/\10/g' \
     -e 's/:0 0( 0 0)?(;|})/:0\2/g' \
-    -e 's,url\("([[:alnum:]\./_-]*)"\),url(\1),g' \
+    -e 's,url\("([[:alnum:]/_.-]*)"\),url(\1),g' \
     -e 's/([ :,])0\.([0-9]+)/\1.\2/g'
 
 
@@ -124,11 +162,5 @@ sed -E \
 # sed 's/^[^{]*//' min.css | sort | uniq -cid | sort -r
 
 
-for name in "${NAMES[@]}"; do
-	pngcrush -rem allb -brute -reduce "$name" _.png >/dev/null && mv -f _.png "$name"
-done
-
-#pngcrush -rem allb -brute -reduce original.png optimized.png
-#optipng -o7 original.png
 
 
